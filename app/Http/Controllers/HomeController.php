0ae68d5c72940b330013ca0fe19fa73096acc42f
FrancescoSica16 <francesco.sica16@gmail.com>
2021-11-04 00:02:46 +0100

    add Model folder for Movie

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,13 +2,15 @@
 
 namespace App\Http\Controllers;
 
-use App\Movie;
+use App\Models\Movie;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
     public function index(){
-        $movies = Movie::all(); // ???      
+        $movies = Movie::all(); // metotodo statico della classe model che la classe mouvie sta estendendo, perche movie eredita tutti i motodi della classe model       
         return view("home", compact("movies"));
     }
+
+    
 }
